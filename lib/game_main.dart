44b85bdaecc712412152b2338c14791f4081961c
SyedAhmedRazaZaidi <115361257+SyedAhmedRazaZaidi@users.noreply.github.com>
2023-06-03 09:54:57 +0500

    Update game_main.dart

--- lib/game_main.dart
+++ lib/game_main.dart
@@ -16,83 +16,94 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 30, left: 10),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: IconButton(
-                onPressed: () {
-                  // Add functionality for back button
-                },
-                icon: FaIcon(FontAwesomeIcons.arrowLeft),
-              ),
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF304FFE), Color(0xFF0000FF)],
           ),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Text(
-              "Welcome To CyberQuest Game",
-              style: GoogleFonts.adventPro(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            "Select your role",
-            style: GoogleFonts.adventPro(
-              fontSize: 26,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 30),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                CupertinoPageRoute(
-                  builder: (context) => MissionSelectionScreen('Security Analyst'),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 30, left: 10),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                  onPressed: () {
+                    // Add functionality for back button
+                  },
+                  icon: FaIcon(FontAwesomeIcons.arrowLeft),
                 ),
-              );
-            },
-            child: Padding(
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                "Welcome To CyberQuest Game",
+                style: GoogleFonts.adventPro(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Select your role",
+              style: GoogleFonts.adventPro(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 30),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (context) => MissionSelectionScreen('Security Analyst'),
+                  ),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ButtonWidget(
+                  'Security Analyst',
+                  Colors.blueGrey,
+                  Colors.black,
+                  500,
+                  100,
+                ),
+              ),
+            ),
+            Padding(
               padding: const EdgeInsets.all(8.0),
               child: ButtonWidget(
-                'Security Analyst',
-                Colors.blueGrey,
+                'Ethical Hacker',
+                Colors.grey,
                 Colors.black,
                 500,
                 100,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ButtonWidget(
-              'Ethical Hacker',
-              Colors.grey,
-              Colors.black,
-              500,
-              100,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ButtonWidget(
+                'Pen Tester',
+                Colors.white30,
+                Colors.black,
+                500,
+                100,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ButtonWidget(
-              'Pen Tester',
-              Colors.white30,
-              Colors.black,
-              500,
-              100,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
