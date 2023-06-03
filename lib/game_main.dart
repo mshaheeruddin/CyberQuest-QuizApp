61f7785a1d64981b27ec2d653dda6064a96cc742
SyedAhmedRazaZaidi <115361257+SyedAhmedRazaZaidi@users.noreply.github.com>
2023-06-03 08:07:16 +0500

    Update game_main.dart

--- lib/game_main.dart
+++ lib/game_main.dart
@@ -16,54 +16,82 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Stack(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 30),
-                child: IconButton(onPressed: () {
-                }, icon: FaIcon(FontAwesomeIcons.arrowLeft)),
-              )
-            ],
-          ),
           Padding(
-            padding: const EdgeInsets.only(top: 20.0, left: 14),
+            padding: const EdgeInsets.only(top: 30, left: 10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                onPressed: () {
+                  // Add functionality for back button
+                },
+                icon: FaIcon(FontAwesomeIcons.arrowLeft),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
             child: Text(
               "Welcome To CyberQuest Game",
-              style: GoogleFonts.adventPro(fontSize: 30),
+              style: GoogleFonts.adventPro(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0, left: 20),
-            child: Text(
-              "Select your role",
-              style: GoogleFonts.adventPro(fontSize: 26, fontWeight: FontWeight.bold),
+          SizedBox(height: 20),
+          Text(
+            "Select your role",
+            style: GoogleFonts.adventPro(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          SizedBox(height: 30,),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      CupertinoPageRoute(builder: (context) => MissionSelectionScreen('Security Analyst')));
-                },
-                child: ButtonWidget('Security Analyst', Colors.blueGrey,Colors.black, 500,100)),
+          SizedBox(height: 30),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) => MissionSelectionScreen('Security Analyst'),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ButtonWidget(
+                'Security Analyst',
+                Colors.blueGrey,
+                Colors.black,
+                500,
+                100,
+              ),
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ButtonWidget('Ethical Hacker', Colors.grey,Colors.black, 500,100),
+            child: ButtonWidget(
+              'Ethical Hacker',
+              Colors.grey,
+              Colors.black,
+              500,
+              100,
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ButtonWidget('Pen Tester', Colors.white30,Colors.black, 500,100),
-          )
-
+            child: ButtonWidget(
+              'Pen Tester',
+              Colors.white30,
+              Colors.black,
+              500,
+              100,
+            ),
+          ),
         ],
       ),
     );
